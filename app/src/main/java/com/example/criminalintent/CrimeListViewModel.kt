4b46dc63bd858ec04def937d3d4e650130095c61
Constantin Gabunia <bananadungeon@gmail.com>
2022-05-22 21:33:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.criminalintent
+
+import androidx.lifecycle.ViewModel
+import java.io.File
+
+class CrimeListViewModel : ViewModel() {
+
+//    val crimes = mutableListOf<Crime>()
+//
+//    init {
+//        for (i in 0 until 100){
+//            val crime = Crime()
+//            crime.title = "Crime #$i"
+//            crime.isSolved = i % 2 == 0
+//            crimes += crime
+//        }
+//    }
+
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
+
+}
